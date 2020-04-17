ab327b196e45c73bf9387c2c6f7d4ffd98bc60c0
eduinvestor <eduinvestor100@gmail.com>
2020-04-17 19:50:40 +0200

    create VS project

---
+++ full_code/Motor2D/j1Scene.cpp
@@ -0,0 +1,128 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Audio.h"
+#include "j1Render.h"
+#include "j1Window.h"
+#include "j1Map.h"
+#include "j1Scene.h"
+
+j1Scene::j1Scene() : j1Module()
+{
+	name.create("scene");
+	test_rect = { 400,400,20,40 };
+	going_up = true;
+}
+
+// Destructor
+j1Scene::~j1Scene()
+{}
+
+// Called before render is available
+bool j1Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1Scene::Start()
+{
+	App->map->Load("iso_walk.tmx");
+
+	debug_tex = App->tex->Load("maps/path2.png");
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PreUpdate()
+{
+
+	// debug pathfing ------------------
+	static iPoint origin;
+	static bool origin_selected = false;
+
+	int x, y;
+	App->input->GetMousePosition(x, y);
+	iPoint p = App->render->ScreenToWorld(x, y);
+	p = App->map->WorldToMap(p.x, p.y);
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::Update(float dt)
+{
+	// Gui ---
+	
+	// -------
+	if(App->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
+		App->LoadGame("save_game.xml");
+
+	if(App->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
+		App->SaveGame("save_game.xml");
+
+	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		App->render->camera.y += floor(200.0f * dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		App->render->camera.y -= floor(200.0f * dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		App->render->camera.x += floor(200.0f * dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		App->render->camera.x -= floor(200.0f * dt);
+
+	App->map->Draw();
+
+	int x, y;
+	App->input->GetMousePosition(x, y);
+	iPoint map_coordinates = App->map->WorldToMap(x - App->render->camera.x, y - App->render->camera.y);
+	p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d Tile:%d,%d",
+		App->map->data.width, App->map->data.height,
+		App->map->data.tile_width, App->map->data.tile_height,
+		App->map->data.tilesets.count(),
+		map_coordinates.x, map_coordinates.y);
+
+	//test_rect movement
+	if (test_rect.y < 250)
+		going_up = false;
+	if (test_rect.y > 550)
+		going_up = true;
+
+	if (going_up)
+		test_rect.y--;
+	else
+		test_rect.y++;
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PostUpdate()
+{
+	bool ret = true;
+	if(App->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	App->render->DrawQuad(test_rect, 255, 0, 0, 255);
+	static char title[256];
+	sprintf_s(title, 256, "Minimaps Personal Research - Quad position: x: %i y: %i", test_rect.x, test_rect.y);
+	App->win->SetTitle(title);
+
+	return ret;
+}
+
+// Called before quitting
+bool j1Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
